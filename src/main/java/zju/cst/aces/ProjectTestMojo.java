0f28834fb5ce19f2dcbe67789561c1c9c1d30d1d
zzzghttt <1800013203@pku.edu.cn>
2024-03-15 19:39:22 +0800

    Add pluginSign

--- src/main/java/zju/cst/aces/ProjectTestMojo.java
+++ src/main/java/zju/cst/aces/ProjectTestMojo.java
@@ -161,6 +161,7 @@ public class ProjectTestMojo
                 .frequencyPenalty(frequencyPenalty)
                 .presencePenalty(presencePenalty)
                 .proxy(proxy)
+                .pluginSign("ChatUniTest")
                 .build();
         config.print();
     }
